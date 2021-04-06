a8c5970bb2c93d231c57c30f07274dce978eccbb
Yash Agrawal <80036700+YashAgrawal213@users.noreply.github.com>
2021-04-06 10:54:05 +0530

    Add files via upload

---
+++ sum_of_n_natural_numbers.py
@@ -0,0 +1,5 @@
+x=int(input("enter the range"))
+s=0
+for k in range(1,x+1):
+    s=s+k
+print(f'natural sum up to {x} is {s}')
---
+++ sum_of_natural_numbers_between_the_given_range.py
@@ -0,0 +1,6 @@
+x=int(input("enter starting number:"))
+y=int(input("enter ending number:"))
+z=0
+for k in range(x,y+1):
+    z=z+k
+print(z)
