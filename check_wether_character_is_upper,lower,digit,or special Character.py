8748e58fd11cbfeca1bdcf994389ce9807eb9536
Yash Agrawal <80036700+YashAgrawal213@users.noreply.github.com>
2021-04-06 10:58:44 +0530

    Add files via upload

---
+++ check_wether_character_is_upper,lower,digit,or special Character.py
@@ -0,0 +1,9 @@
+x=input("enter character")
+if x>="A" and x<="Z":
+    print("You Entered An Upper Case Letter")
+elif x>="a" and x<="z":
+    print("You Entered An Lower Case Letter")
+elif x>="0" and x<="9":
+    print("You Entered An Integer")
+else:
+    print("You Entered A Special Character")
---
+++ print_numeric_table_of_given_integer.py
@@ -0,0 +1,3 @@
+x= int(input("enter the number:"))
+for k in range(1,11):
+    print(x,"x",k,"=",x*k)
