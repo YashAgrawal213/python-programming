28922699231304f25548bdb4de24c57153bc6a65
Yash Agrawal <80036700+YashAgrawal213@users.noreply.github.com>
2022-02-12 14:30:37 +0530

    Add files via upload
    sequences

---
+++ Write Python programs to print the series  1,4,7,10,......,n.py
@@ -0,0 +1,3 @@
+n = int(input("enter the last number"))
+for k in range(1,n+1,3):
+    print(k,end=" ")
---
+++ Write Python programs to sum the sequences 1^2+3^2+5^2+....+n^2.py
@@ -0,0 +1,5 @@
+m=0
+n=int(input("enter the end number"))
+for k in range(1,n+1,2):
+    m=m+(k**2)
+print("the sum of the series is",m)
